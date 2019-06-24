cb2a9dca8480a565681fdc114330fe1a898eef29
Raruto <Theo.95@hotmail.it>
2019-06-25 01:40:46 +0200

    replace default controls with "leaflet-ui"

--- src/app.js
+++ src/app.js
@@ -1,147 +1,69 @@
 import css from './app.css';
 
-import * as G from 'google-maps'; // async
-
-import JSZip from 'jszip';
-import toGeoJSON from '@tmcw/togeojson';
-import geojsonvt from 'geojson-vt';
-
+import 'leaflet-ui';
 import 'leaflet-hash';
-import 'leaflet.gridlayer.googlemutant';
-import 'leaflet.locatecontrol';
-import 'leaflet.fullscreen';
-import 'leaflet-pegman';
-import 'leaflet-transparency';
-import '@raruto/leaflet-gesture-handling';
-import 'leaflet-control-layers-inline';
 import 'leaflet-kmz';
+import 'leaflet-transparency';
 
-window.JSZip = JSZip;
-window.toGeoJSON = toGeoJSON;
-window.geojsonvt = geojsonvt;
+var map = new L.Map('map', {
+  center: [41.4583, 12.7059],
+  zoom: 5,
+  gestureHandling: true,
+  mapTypeId: 'topo',
+  mapTypeIds: ['osm', 'terrain', 'satellite', 'topo'],
+});
 
-if (!window.google)
-  G.load(function(google) {
-    window.google = google;
-    initMap();
-  });
-else
-  initMap();
+map.once('idle', function() {
 
-function initMap() {
-  var map, hash;
-  var layers = {};
-  var controls = {};
+  var hash = new L.Hash(map);
 
-  map = new L.Map('map', {
-    center: [41.4583, 12.7059],
-    zoom: 5,
-    zoomControl: false,
-    gestureHandling: true,
-  });
-
-  hash = new L.Hash(map);
-
-  layers.osm = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  });
-
-  layers.otm = new L.TileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-  });
-
-  layers.google = {};
-
-  layers.google.terrain = new L.GridLayer.GoogleMutant({
-    type: 'terrain',
-    maxZoom: 24,
-  });
-
-  layers.google.satellite = new L.GridLayer.GoogleMutant({
-    type: 'satellite',
-    maxZoom: 24,
-  });
-
-  var baseMaps = {
-    "OSM": layers.osm,
-    "Terrain": layers.google.terrain,
-    "Satellite": layers.google.satellite,
-    "Topo": layers.otm,
-  };
-
-  controls.baseLayers = new L.Control.Layers(baseMaps, null, {
-    inline: true,
-    position: 'topleft'
-  });
-
-  controls.layers = new L.Control.Layers(null, null, {
-    collapsed: false,
-    position: 'topright'
-  });
-
-  controls.zoom = new L.Control.Zoom({
-    position: 'bottomright',
-  });
-
-  controls.pegman = new L.Control.Pegman({
-    position: 'bottomright',
-    theme: "leaflet-pegman-v3-small",
-  });
-
-  controls.locate = new L.Control.Locate({
-    position: "bottomright"
-  });
-
-  controls.fullscreen = new L.Control.FullScreen({
-    position: 'topright',
-    title: 'Enter Fullscreen',
-    titleCancel: 'Exit Fullscreen',
-    forceSeparateButton: true,
-  });
-
-  controls.opacity = new L.Control.OpacitySlider(null, {
+  var layersOpacity = new L.Control.OpacitySlider(null, {
     sliderImageUrl: "https://unpkg.com/leaflet-transparency@0.0.3/images/opacity-slider2.png",
     backgroundColor: "rgba(229, 227, 223, 0.9)",
     position: 'topleft',
-  });
-
-  var leafletAttribution = map.attributionControl.options.prefix;
-  map.on('baselayerchange', updateLeafletAttribution);
-
-  for (var i in controls) {
-    if (controls[i].addTo) {
-      controls[i].addTo(map);
-    }
-  }
-
-  layers.otm.addTo(map);
+  }).addTo(map);
 
   // Instantiate KMZ parser (async)
   var kmzParser = new L.KMZParser({
-    onKMZLoaded: function(layer, name) {
-      controls.layers.addOverlay(layer, name);
-      layer.addTo(map);
-      layer.bringToFront();
-      map.on('baselayerchange', function(e) {
-        layer.bringToFront();
-      });
-
-      var layersOpacity = new L.Control.OpacitySlider(this.gridlayer, {
-        sliderImageUrl: "https://unpkg.com/leaflet-transparency@0.0.3/images/opacity-slider2.png",
-        backgroundColor: "rgba(229, 227, 223, 0.9)",
-        position: 'topright',
-      });
-      layersOpacity.addFeatureLayer(this.layer);
-      layersOpacity.addTo(map);
-    },
+    onKMZLoaded: onKMZLoaded,
     name: "ON / OFF"
   });
+
   kmzParser.load('regions.kmz');
 
-  function updateLeafletAttribution(e) {
-    map.attributionControl.setPrefix((e && e.layer && e.layer instanceof L.GridLayer.GoogleMutant) ? false : leafletAttribution);
+});
+
+
+function onKMZLoaded(layer, name) {
+
+  layer.addTo(map);
+  overlayControl(this.layer, this.name);
+  opacitySlider(this.layer, this.gridlayer);
+  keepFront(this.layer);
+
+  function keepFront(layer) {
+    layer.bringToFront();
+    map.on('baselayerchange', function(e) {
+      if (layer.bringToFront) layer.bringToFront();
+    });
   }
 
+  function overlayControl(layer, name) {
+    var layersControl = new L.Control.Layers(null, null, {
+      collapsed: false,
+      position: 'topright'
+    });
+    layersControl.addOverlay(layer, name);
+    layersControl.addTo(map);
+  }
+
+  function opacitySlider(layer, gridlayer) {
+    var layersOpacity = new L.Control.OpacitySlider(gridlayer, {
+      sliderImageUrl: "https://unpkg.com/leaflet-transparency@0.0.3/images/opacity-slider2.png",
+      backgroundColor: "rgba(229, 227, 223, 0.9)",
+      position: 'topright',
+    });
+    layersOpacity.addFeatureLayer(layer);
+    layersOpacity.addTo(map);
+  }
 }
